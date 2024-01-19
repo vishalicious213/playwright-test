a1dba1f5be6e153b742f04d775734ab9308e58a6
vishalicious213 <vishalicious213@gmail.com>
2024-01-19 09:41:17 -0800

    drafted index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// testing CSV generation
+
+// create csv data
+const csvData = [
+    ['Name', 'Age', 'City'],
+    ['John', 25, 'New York'],
+    ['Jane', 30, 'San Francisco'],
+    ['Bob', 22, 'Los Angeles']
+];
