9e56d274f01b5fbe6dec235c7c524f98ff2d7b16
vishalicious213 <vishalicious213@gmail.com>
2024-01-19 13:35:48 -0800

    scraping titles and url's from hacker news articles

--- hacker-news.js
+++ hacker-news.js
@@ -2,15 +2,34 @@
 const { chromium } = require("playwright");
 
 async function saveHackerNewsArticles() {
-  // launch browser
-  const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
-  const page = await context.newPage();
+    // launch browser
+    const browser = await chromium.launch({ headless: false })
+    const context = await browser.newContext()
+    const page = await context.newPage()
 
-  // go to Hacker News
-  await page.goto("https://news.ycombinator.com");
+    // go to Hacker News
+    await page.goto("https://news.ycombinator.com")
+
+    // get titles and urls
+    const articles = await page.evaluate(() => {
+        const articleData = document.querySelectorAll(".titleline a")
+        const articlesArray = []
+        // console.log(articleData)
+
+        for (const article of articleData) {
+            const title = article.innerText
+            const url = article.href
+            const unneeded = article.childNodes[0].firstChild
+            // console.table(title, url, plop)
+            if (!unneeded) {
+                articlesArray.push([title, url])
+            }
+        }
+
+        console.log(articlesArray)
+    })
 }
 
 (async () => {
-  await saveHackerNewsArticles();
+    await saveHackerNewsArticles()
 })();
