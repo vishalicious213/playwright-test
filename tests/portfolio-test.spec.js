89917a5dbd2370952ae6ee43a7e8820f763b4d7e
vishalicious213 <vishalicious213@gmail.com>
2024-01-18 17:33:04 -0800

    drafted /tests/portfolio-test.spec.js and ran it successfully

--- tests/portfolio-test.spec.js
+++ tests/portfolio-test.spec.js
@@ -1,10 +1,13 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
+const { test, expect } = require("@playwright/test")
 
-test('Portfolio test', async ({ page }) => {
+test('test', async ({ page }) => {
   await page.goto('about:blank');
-  await page.goto('chrome-error://chromewebdata/');
   await page.goto('https://vish213-portfolio-v3.netlify.app/');
   await page.getByRole('button', { name: 'Front-End Development' }).click();
   await page.getByRole('button', { name: 'Back-End Development' }).click();
   await page.getByRole('img', { name: 'Back of Vish\'s head, holding' }).click();
+  const page1Promise = page.waitForEvent('popup');
+  await page.getByRole('link', { name: 'BloomTech: Full-Stack Web' }).click();
+  const page1 = await page1Promise;
 });
