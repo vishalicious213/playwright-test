edb2a55ff454247e8eabdb1531d085f9a833baf3
vishalicious213 <vishalicious213@gmail.com>
2024-01-21 11:23:43 -0800

    added 'is this hacker news' test to /tests/initial-test

--- tests/initial-test.spec.js
+++ tests/initial-test.spec.js
@@ -4,3 +4,8 @@ test("Is this Google?", async ({ page }) => {
     await page.goto("https://google.com")
     await expect(page).toHaveTitle("Google")
 })
+
+test("Is this Hacker News?", async ({ page }) => {
+    await page.goto("https://news.ycombinator.com")
+    await expect(page).toHaveTitle("Hacker News")
+})
